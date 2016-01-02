a39e1ce4a1e884a0bea387226bfd3ca5bbe6fd70
atelierbram <bramdeh@gmail.com>
2016-01-02 19:48:02 +0100

    first commit for Duotones

---
+++ duotones/output/mate-terminal/base16-duotone.dark.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Base16 Duotone - Mate Terminal color scheme install script
+# adapted from: Simurai (https://github.com/simurai/duotone-dark-syntax/)
+
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="Base 16 Duotone Dark"
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="base-16-duotone-dark"
+[[ -z "$DCONFTOOL" ]] && DCONFTOOL=dconf
+[[ -z "$BASE_KEY" ]] && BASE_KEY=/org/mate/terminal/profiles
+
+PROFILE_KEY="$BASE_KEY/$PROFILE_SLUG"
+
+dset() {
+  local key="$1"; shift
+  local val="$1"; shift
+
+  "$DCONFTOOL" write "$PROFILE_KEY/$key" "$val"
+}
+
+# Because gconftool doesn't have "append"
+glist_append() {
+  local key="$1"; shift
+  local val="$1"; shift
+
+  local entries="$(
+    {
+      "$DCONFTOOL" read "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+      echo "'$val'"
+    } | head -c-1 | tr "\n" ,
+  )"
+
+  "$DCONFTOOL" write "$key" "[$entries]"
+}
+
+# Append the Base16 profile to the profile list
+glist_append /org/mate/terminal/global/profile-list "$PROFILE_SLUG"
+
+dset visible-name "'$PROFILE_NAME'"
+dset palette "'#2a2734:#7769bf:#7c756e:#ce853b:#d49454:#988dce:#daa36c:#a391fd:#746e91:#7769bf:#7c756e:#ce853b:#d49454:#988dce:#daa36c:#eeebff'"
+dset background-color "'#2a2734'"
+dset foreground-color "'#a391fd'"
+dset bold-color "'#a391fd'"
+dset bold-color-same-as-fg "true"
+dset use-theme-colors "false"
+dset use-theme-background "false"
---
+++ duotones/output/rouge/base16-duotone.rb
@@ -0,0 +1,26 @@
+module Rouge
+  module Themes
+    class Duotone < Base16
+      name 'base16.Duotone'
+      # author adapted from: Simurai (https://github.com/simurai/duotone-dark-syntax/)
+      light!
+
+      palette base00: "#2a2734"
+      palette base01: "#413d51"
+      palette base02: "#5c5874"
+      palette base03: "#746e91"
+      palette base04: "#857ab8"
+      palette base05: "#a391fd"
+      palette base06: "#c4b8fe"
+      palette base07: "#eeebff"
+      palette base08: "#7769bf"
+      palette base09: "#c07830"
+      palette base0A: "#ce853b"
+      palette base0B: "#7c756e"
+      palette base0C: "#daa36c"
+      palette base0D: "#d49454"
+      palette base0E: "#988dce"
+      palette base0F: "#877bc6"
+    end
+  end
+end
