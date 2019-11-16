49f27aa5aacd3ef0467081225890d7580bb92527
atelierbram <bramdeh@gmail.com>
2019-11-16 20:35:00 +0100

    added link to base4tone-alacritty repo

--- src/base4tone/index.php
+++ src/base4tone/index.php
@@ -22,6 +22,7 @@ include '../inc/nav.inc';
 <p class="mtl">Base4Tone is an opinionated system for working with variables in webdesign, and is also used to generate a collection of colorthemes for many different applications in dark - and light versions. Applications like:</p>
 <li><a href="https://github.com/atelierbram/Base4Tone-iterm2/">iTerm2</a></li>
 <li><a href="https://github.com/atelierbram/Base4Tone-hyper/">Hyper</a></li>
+<li><a href="https://github.com/atelierbram/Base4Tone-alacritty/">Alacritty</a></li>
 <li><a href="https://github.com/atelierbram/Base4Tone-vim/">Vim</a></li>
 <li><a href="https://github.com/atelierbram/Base4Tone-VSCode-Themes">VSCode</a></li>
 <li><a href="https://github.com/atelierbram/Base4Tone-prism/">Prism</a></li>
